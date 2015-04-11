f56ca54785f1e1dc3422ebd6d85a3e48268dd55a
Keith Hall <kingkeith+github@gmail.com>
2015-04-11 11:42:33 +0300

    Create ObservableExtensions.cs

---
+++ src/ObservableExtensions.cs
@@ -0,0 +1,7 @@
+public static class ObservableExtensions {
+	public static IObservable<TOutput> WithPrevious<TSource, TOutput> (this IObservable<TSource> source, Func<TSource, TSource, TOutput> projection) {
+		return source.Scan(Tuple.Create(default(TSource), default(TSource)),
+			(previous, current) => Tuple.Create(previous.Item2, current))
+			.Select(t => projection(t.Item1, t.Item2));
+	}
+}
