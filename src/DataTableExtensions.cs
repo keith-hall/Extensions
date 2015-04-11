db7805ec2bb33a9aeaaae796a67c6e1b88004ba6
Keith Hall <kingkeith+github@gmail.com>
2015-04-11 11:58:13 +0300

    Create DataTableExtensions.cs

---
+++ src/DataTableExtensions.cs
@@ -0,0 +1,88 @@
+public static class DataTableExtensions {
+	public static string TableToSQLInsert (this DataTable dt, string tableName, bool createTable) {
+		var sql = "insert into " + tableName + " (" +
+			string.Join(", ", dt.Columns.OfType<DataColumn>().Select(
+				col => "[" + col.ColumnName + "]"
+			)) + ") values\r\n";
+		float tmp;
+		sql += " (" + string.Join(")\r\n,(", dt.Rows.OfType<DataRow>().Select(
+			row => string.Join(", ", dt.Columns.OfType<DataColumn>().Select(
+				col => (row[col].ToString().Length < 7 && float.TryParse(row[col].ToString(), out tmp)) ? row[col].ToString() : ("'" + row[col].ToString().Replace("'", "''") + "'") // determine if number or not
+			)))
+		) + ")";
+		
+		if (createTable) {
+			sql = (table.StartsWith("@") ? "DECLARE " : "CREATE TABLE ") + tableName + (tableName.StartsWith("@") ? " TABLE " : string.Empty) + " (" + string.Join(
+				", ",
+				dt.Columns.OfType<DataColumn>().Select(dc => "[" + dc.ColumnName + "] NVARCHAR(" + dt.Rows.OfType<DataRow>().Max(dr => dr[dc].ToString().Length).ToString() + ")")
+				) + ")\r\n" + sql;
+		}
+		
+		return sql;
+	}
+	
+	public static DataTable Filter (this DataTable table, string filter) {
+		return table.Filter(table.Select(filter));
+	}
+	
+	public static DataTable Filter (this DataTable table, IEnumerable<DataRow> rows) {
+		var filtered = table.Clone();
+		foreach (var row in rows)
+			filtered.Rows.Add(row.ItemArray);
+		return filtered;
+	}
+	
+	public static DataTable ReadXML (XmlReader xr, string row, bool shortNames, bool includeAttributes) {
+		var stack = new Stack<string>();
+		var dt = new DataTable();
+		DataRow currentRow = null;
+		
+		Action process = () => {
+			if (xr.HasValue && ! string.IsNullOrWhiteSpace(xr.Value) && xr.NodeType != XmlNodeType.Comment) {
+				var col = string.Join("/", stack);
+				if (! dt.Columns.Contains(col))
+					dt.Columns.Add(col);
+				
+				if (currentRow != null)
+					currentRow.SetField(col, xr.Value);
+			}
+			if (xr.NodeType == XmlNodeType.EndElement ||
+				(xr.NodeType == XmlNodeType.Element && xr.IsEmptyElement) ||
+				xr.NodeType == XmlNodeType.Attribute)
+				stack.Pop();
+		};
+		
+		while (xr.Read()) {
+			if (xr.NodeType == XmlNodeType.Element) {
+				if (String.IsNullOrEmpty(dt.TableName))
+					dt.TableName = xr.Name;
+				
+				stack.Push(xr.Name);
+				if (xr.Name == row) {
+					currentRow = dt.NewRow();
+					dt.Rows.Add(currentRow);
+				}
+			}
+			if (xr.HasAttributes && includeAttributes) {
+				xr.MoveToFirstAttribute();
+				do {
+					stack.Push(stack.Peek() + "@" + xr.Name);
+					process();
+				} while (xr.MoveToNextAttribute());
+				xr.MoveToElement();
+			}
+			process();
+		}
+		xr.Close();
+		
+		if (shortNames) {
+			var cols = dt.Columns.OfType<DataColumn>().Select(dc => new { Column = dc, FullName = dc.ColumnName, ShortName = dc.ColumnName.Split('/').First() }).GroupBy(c => c.ShortName).Where(g => g.Count() == 1).Select (g => g.First());
+			foreach (var col in cols)
+				//try {
+					col.Column.ColumnName = col.ShortName;
+				//} catch (DuplicateNameException) { // TODO: instead, only rename the columns that won't clash
+				//}
+		}
+		return dt;
+	}
+}
