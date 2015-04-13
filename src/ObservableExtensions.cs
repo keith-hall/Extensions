1098b6ccef333b4ae2abb0b4e0dc2f48ce906ca6
Keith Hall <kingkeith+github@gmail.com>
2015-04-13 16:09:31 +0300

    Update ObservableExtensions.cs
    added usings

--- src/ObservableExtensions.cs
+++ src/ObservableExtensions.cs
@@ -1,5 +1,10 @@
-public static class ObservableExtensions {
-	public static IObservable<TOutput> WithPrevious<TSource, TOutput> (this IObservable<TSource> source, Func<TSource, TSource, TOutput> projection) {
+using System;
+using System.Reactive.Linq;
+
+public static class ObservableExtensions
+{
+	public static IObservable<TOutput> WithPrevious<TSource, TOutput>(this IObservable<TSource> source, Func<TSource, TSource, TOutput> projection)
+	{
 		return source.Scan(Tuple.Create(default(TSource), default(TSource)),
 			(previous, current) => Tuple.Create(previous.Item2, current))
 			.Select(t => projection(t.Item1, t.Item2));
