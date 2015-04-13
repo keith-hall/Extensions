973fef8ec5bdfe1bf63dd4fc43a6dabd83ac80ab
Keith Hall <kingkeith+github@gmail.com>
2015-04-13 06:50:51 +0300

    Create EnumerableExtensions.cs
    added extension method to sort an IEnumerable in a random order

---
+++ src/EnumerableExtensions.cs
@@ -0,0 +1,15 @@
+public static EnumerableExtensions {
+	public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source, Random rng)
+		{ // http://stackoverflow.com/questions/1287567/is-using-random-and-orderby-a-good-shuffle-algorithm
+			T[] elements = source.ToArray();
+			for (int i = elements.Length - 1; i >= 0; i--) {
+				// Swap element "i" with a random earlier element it (or itself)
+				// ... except we don't really need to swap it fully, as we can
+				// return it immediately, and afterwards it's irrelevant.
+				int swapIndex = rng.Next(i + 1);
+				yield return elements[swapIndex];
+				elements[swapIndex] = elements[i];
+			}
+		}
+	}
+}
