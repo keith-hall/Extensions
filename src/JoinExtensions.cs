514515758ecca76c4f3db3a3c3bc5cde6b4b0a73
Keith Hall <kingkeith+github@gmail.com>
2015-04-14 08:38:06 +0300

    Update JoinExtensions.cs
    added namespace

--- src/JoinExtensions.cs
+++ src/JoinExtensions.cs
@@ -2,22 +2,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-public static class JoinExtensions
+namespace HallLibrary.Extensions
 {
-	public static IEnumerable<Tuple<TLeft, TRight>> LeftJoinEach<TLeft, TRight, TJoin>(this IEnumerable<TLeft> left, IEnumerable<TRight> right, Func<TLeft, TJoin> leftJoinOn, Func<TRight, TJoin> rightJoinOn)
+	public static class JoinExtensions
 	{
-		var j = from l  in left
-			join r  in right on leftJoinOn(l) equals rightJoinOn(r) into gj
-			from jr in gj.DefaultIfEmpty()
-			select Tuple.Create(l, jr);
-		return j;
-	}
-
-	public static IEnumerable<Tuple<TLeft, IEnumerable<TRight>>> LeftJoinAll<TLeft, TRight, TJoin>(this IEnumerable<TLeft> left, IEnumerable<TRight> right, Func<TLeft, TJoin> leftJoinOn, Func<TRight, TJoin> rightJoinOn)
-	{
-		var j = from l in left
-			join r in right on leftJoinOn(l) equals rightJoinOn(r) into gj
-			select Tuple.Create(l, gj.DefaultIfEmpty());
-		return j;
+		public static IEnumerable<Tuple<TLeft, TRight>> LeftJoinEach<TLeft, TRight, TJoin>(this IEnumerable<TLeft> left, IEnumerable<TRight> right, Func<TLeft, TJoin> leftJoinOn, Func<TRight, TJoin> rightJoinOn)
+		{
+			var j = from l  in left
+				join r  in right on leftJoinOn(l) equals rightJoinOn(r) into gj
+				from jr in gj.DefaultIfEmpty()
+				select Tuple.Create(l, jr);
+			return j;
+		}
+	
+		public static IEnumerable<Tuple<TLeft, IEnumerable<TRight>>> LeftJoinAll<TLeft, TRight, TJoin>(this IEnumerable<TLeft> left, IEnumerable<TRight> right, Func<TLeft, TJoin> leftJoinOn, Func<TRight, TJoin> rightJoinOn)
+		{
+			var j = from l in left
+				join r in right on leftJoinOn(l) equals rightJoinOn(r) into gj
+				select Tuple.Create(l, gj.DefaultIfEmpty());
+			return j;
+		}
 	}
 }
