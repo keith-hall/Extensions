0d88a7f6edaa4481b93130e156134cd67a2755d8
Keith Hall <kingkeith+github@gmail.com>
2015-04-15 17:11:56 +0300

    Update EnumerableTests.cs

--- tests/EnumerableTests.cs
+++ tests/EnumerableTests.cs
@@ -15,6 +15,8 @@ namespace Tests
 		{
 			var src = Enumerable.Range(1, 4).Concat(new [] { 0 });
 			Assert.AreEqual(src.Select(s => 1 / s).CountExceeds(3), true);
+			
+			// just doing a Count would give a DivideByZero exception
 		}
 	}
 }
