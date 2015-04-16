be437d2631ee040e73bf03e055575550323068c0
Keith Hall <kingkeith+github@gmail.com>
2015-04-16 14:46:11 +0300

    Create WcfExtensions.cs

---
+++ src/WcfExtensions.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+
+namespace HallLibrary.Extensions {
+	public static class WcfExtensions {
+		public static TReturn CallWCFMethodWithHeaders<TChannel, TReturn> (TChannel channel, IEnumerable<System.ServiceModel.Channels.MessageHeader> headers, Func<TChannel, TReturn> callWCFMethod) {
+			using (var ocs = new OperationContextScope((System.ServiceModel.IContextChannel)channel)) {
+				foreach (var header in headers)
+					OperationContext.Current.OutgoingMessageHeaders.Add(header);
+				return callWCFMethod(channel);
+			}
+		}
+		
+		public static void CallWCFMethodWithHeaders<TChannel> (TChannel channel, IEnumerable<System.ServiceModel.Channels.MessageHeader> headers, Action<TChannel> callWCFMethod) {
+			CallWCFMethodWithHeaders(channel, headers, ch => { callWCFMethod(ch); return 0; });
+		}
+		
+		public static System.ServiceModel.Channels.MessageHeader CreateWCFHeader<T> (string name, T value, string nameSpace = null) {
+			return new System.ServiceModel.MessageHeader<T>(value).GetUntypedHeader(name, nameSpace ?? string.Empty);
+		}
+	}
+}
