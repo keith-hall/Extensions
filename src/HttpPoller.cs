08e5de11b7426a4ee330313936f30e725f816651
Keith Hall <kingkeith+github@gmail.com>
2015-04-22 21:13:18 +0300

    Update HttpPoller.cs
    added xml comments

--- src/HttpPoller.cs
+++ src/HttpPoller.cs
@@ -11,7 +11,14 @@ namespace HallLibrary.Extensions
 			public WebHeaderCollection Headers;
 			public string Text;
 		}
-	
+		
+		/// <summary>
+		/// Poll a <paramref name="url"/> at the specified <paramref name="frequency"/> and create an observable that will update every time the content recieved changes.
+		/// </summary>
+		/// <param name="url">The url to poll.</param>
+		/// <param name="frequency">The frequency to poll at.</param>
+		/// <param name="createWebClient">An optional function to create a WebClient instance, for overriding timeouts, request headers etc.</param>
+		/// <returns>An observable containing the response headers and content of the specified url.</returns>
 		public static IObservable<ResponseDetails> PollURL(string url, TimeSpan frequency, Func<WebClient> createWebClient = null)
 		{
 			createWebClient = createWebClient ?? (() => new WebClient());
