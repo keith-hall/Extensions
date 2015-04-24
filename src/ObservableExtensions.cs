4c6791dffd1635767f7add5b3e290ca92bc243d0
Keith Hall <kingkeith+github@gmail.com>
2015-04-24 14:43:11 +0300

    Update ObservableExtensions.cs

--- src/ObservableExtensions.cs
+++ src/ObservableExtensions.cs
@@ -3,6 +3,9 @@ using System.Reactive.Linq;
 
 namespace HallLibrary.Extensions
 {
+	/// <summary>
+	/// Contains extension methods for working with observables.
+	/// </summary>
 	public static class ObservableExtensions
 	{
 		public static IObservable<TOutput> WithPrevious<TSource, TOutput>(this IObservable<TSource> source, Func<TSource, TSource, TOutput> projection)
