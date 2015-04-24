e5abb23b0b7eddb14a341f3ca1446ebdcf378b5b
Keith Hall <kingkeith+github@gmail.com>
2015-04-24 14:44:57 +0300

    Update ObservableExtensions.cs

--- src/ObservableExtensions.cs
+++ src/ObservableExtensions.cs
@@ -8,6 +8,14 @@ namespace HallLibrary.Extensions
 	/// </summary>
 	public static class ObservableExtensions
 	{
+		/// <summary>
+		/// Returns the current value of an observable with the previous value.
+		/// </summary>
+		/// <typeparam name="TSource">The source type.</typeparam>
+		/// <typeparam name="TOutput">The output type after the <paramref name="projection" /> has been applied.</typeparam>
+		/// <param name="source">The observable.</param>
+		/// <param name="projection">The projection to apply.</param>
+		/// <returns>The current observable value with the previous value.</returns>
 		public static IObservable<TOutput> WithPrevious<TSource, TOutput>(this IObservable<TSource> source, Func<TSource, TSource, TOutput> projection)
 		{
 			return source.Scan(Tuple.Create(default(TSource), default(TSource)),
