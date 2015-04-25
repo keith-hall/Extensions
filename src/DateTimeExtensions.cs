e4e30b4e347f90d0868a3c741c739de5d13b3965
Keith Hall <kingkeith+github@gmail.com>
2015-04-25 21:49:40 +0300

    Update DateTimeExtensions.cs

--- src/DateTimeExtensions.cs
+++ src/DateTimeExtensions.cs
@@ -8,18 +8,31 @@ public static class DateTimeExtensions
 	/// </summary>
 	/// <param name="dt">The date to convert.</param>
 	/// <param name="withT">Whether or not to include the 'T' in the output.  If false, a space will be used instead.</param>
-	/// <param name="toUTC">Whether or not to convert the time to UTC or not first.</param>
 	/// <returns>An ISO-8601 formatted <see cref="String"/> representation of the specified <see cref="DateTime"/>.</returns>
 	public static string ToISO8601String(this DateTime dt, bool withT)
 	{
 		return dt.ToString(
-			Thread.CurrentThread.CurrentCulture.DateTimeFormat.UniversalSortableDateTimePattern
-			.Replace(@"Z'", @".'fff")
+			dt.ToSortableDateTime()
 			.Replace(@" ", withT ? @"'T'" : @" ")
 			+ @"'" + TimeZoneString(dt) + @"'"
 		, CultureInfo.InvariantCulture);
 	}
 	
+	/// <summary>
+	/// Converts the specified <see cref="DateTime"/> to a <see cref="String"/>, in a universal sortable format.
+	/// </summary>
+	/// <param name="dt">The date to convert.</param>
+	/// <returns>A sortable formatted <see cref="String"/> representation of the specified <see cref="DateTime"/>.</returns>
+	public static string ToSortableDateTime (this DateTime dt) {
+		return Thread.CurrentThread.CurrentCulture.DateTimeFormat.UniversalSortableDateTimePattern
+		       .Replace(@"Z'", @".'fff");
+	}
+	
+	/// <summary>
+	/// Gets the local time zone offset <see cref="String"/> from UTC for the specified <see cref="DateTime"/>.
+	/// </summary>
+	/// <param name="dt">The date to get the local timezone offset <see cref="String"/> for.</param>
+	/// <returns>A <see cref="String"/> representation of the local timezone offset for the specified <see cref="DateTime"/>.</returns>
 	public static string TimeZoneString (this DateTime dt) {
 		if (dt.Kind == DateTimeKind.Utc)
 			return @"Z";
@@ -31,7 +44,12 @@ public static class DateTimeExtensions
 		       + @":" + diff.Minutes.ToString(@"00");
 	}
 	
-	public static DateTime FromUtc (this DateTime dt) {
+	/// <summary>
+	/// Convert the specified <see cref="DateTime"/> from UTC to the Local timezone.
+	/// </summary>
+	/// <param name="dt">The date to convert to local time.</param>
+	/// <returns>The specified UTC <see cref="DateTime"/> converted to the Local timezone.</returns>
+	public static DateTime FromUniversalTime (this DateTime dt) {
 		if (dt.Kind == DateTimeKind.Local)
 			return dt;
 		
