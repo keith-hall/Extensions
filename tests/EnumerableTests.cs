a3d1f48d284a8a3ed9ea75fb7e9f0a1aac3b37d1
Keith Hall <kingkeith+github@gmail.com>
2015-05-08 10:32:16 +0300

    Update EnumerableTests.cs
    added more tests to CountExceeds

--- tests/EnumerableTests.cs
+++ tests/EnumerableTests.cs
@@ -13,10 +13,31 @@ namespace Tests
 		[TestMethod]
 		public void TestCountExceeds()
 		{
-			var src = Enumerable.Range(1, 4).Concat(new [] { 0 });
-			Assert.AreEqual(src.Select(s => 1 / s).CountExceeds(3), true);
-			
+			var src = Enumerable.Range(1, 4).Concat(new[] { 0 }).Select(s => 1 / s);
+			// ReSharper disable once PossibleMultipleEnumeration
+			Assert.AreEqual(src.CountExceeds(3), true);
+
 			// just doing a Count would give a DivideByZero exception
+			try
+			{
+				// ReSharper disable once PossibleMultipleEnumeration
+				// ReSharper disable once ConvertIfStatementToConditionalTernaryExpression
+				if (src.Count() > 3)
+					Assert.Fail("The above should cause a DivideByZero Exception so this line should never be reached.");
+				else
+					Assert.Fail("Count is greater than 3 so this line should never be reached.");
+			}
+			catch (DivideByZeroException)
+			{
+
+			}
+			catch (Exception)
+			{
+				Assert.Fail("The above should cause a DivideByZero Exception so this line should never be reached.");
+			}
+			
+			src = Enumerable.Range(1, 2);
+			Assert.IsFalse(src.CountExceeds(2));
 		}
 	}
 }
