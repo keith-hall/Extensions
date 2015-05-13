30d9a62071eefbb9c9b6e30834c5edd0906a41d2
Keith Hall <kingkeith+github@gmail.com>
2015-05-13 09:25:51 +0300

    Update and rename proc_ImportFromFile.sql to proc_ImportCSVFile.sql
    removed specific code to make more generic for importing CSV files to SQL server

--- sql/proc_ImportCSVFile.sql
+++ sql/proc_ImportCSVFile.sql
@@ -1,27 +1,24 @@
-USE [darbine]
+IF OBJECT_ID('proc_ImportCSVFile', N'P') IS NOT NULL
+	EXEC ('DROP PROCEDURE proc_ImportCSVFile')
 GO
-IF OBJECT_ID('proc_ImportCardHashData', N'P') IS NOT NULL
-	EXEC ('DROP PROCEDURE proc_ImportCardHashData')
-GO
-CREATE PROCEDURE proc_ImportCardHashData
-	@FromFile NVARCHAR(1000),
+CREATE PROCEDURE proc_ImportCSVFile
+	@PathToFile NVARCHAR(1000),
 	@Separator CHAR(1) = ';',
 	@Header NVARCHAR(600) = NULL,
 	@IgnoreFileHeader BIT = 0,
-	@CardHashField SYSNAME = 1, -- name of field as per header, or ordinal position
 	@TableName SYSNAME = NULL, -- if provided, stores the imported data in a table with this name
-	@RowSample INT = 0, -- check the data to see if the columns have been imported correctly and the data has the correct characters (i.e. identify charset / character encoding problems...)
-	@ExtraColumns VARCHAR(1000) = NULL,
+	@RowSample INT = 0, -- specify number of rows to load - used to check the data to see if the columns have been imported correctly and the data has the correct characters (i.e. identify charset / character encoding problems...)
+	@ExtraColumns VARCHAR(1000) = NULL, -- useful for when executing SQL when the table already exists, i.e. no late-binding, to avoid errors
 	@DebugMessages BIT = 0
 AS
 
 DECLARE @FileExists INT
 SET NOCOUNT ON
-EXEC master.dbo.xp_fileexist @FromFile, @FileExists OUTPUT
+EXEC master.dbo.xp_fileexist @PathToFile, @FileExists OUTPUT
 SET NOCOUNT OFF
 IF @FileExists = 0
 BEGIN
-	RAISERROR ('File "%s" does not exist', 16, -1, @FromFile)
+	RAISERROR ('File "%s" does not exist.', 16, -1, @PathToFile)
 	RETURN -1
 END
 
@@ -29,7 +26,7 @@ DECLARE @FileContainsHeader BIT = CASE WHEN @Header IS NULL THEN 1 ELSE @IgnoreF
 
 IF @Header IS NULL AND @IgnoreFileHeader = 1
 BEGIN
-	RAISERROR ('No header provided.', 16, -1, @FromFile)
+	RAISERROR ('No header provided.', 16, -1, @PathToFile)
 	RETURN -1
 END
 
@@ -40,11 +37,13 @@ ELSE
 	SAVE TRANSACTION before_import
 
 DECLARE @SQL VARCHAR(MAX)
---DECLARE @DropTable BIT = CASE WHEN @TableName IS NULL THEN 1 ELSE 0 END
 IF @TableName IS NULL
 	SET @TableName = '##ImportedData'
 ELSE IF SUBSTRING(@TableName, 1, 1) = '#' AND NOT SUBSTRING(@TableName, 2, 1) = '#' -- temporary table needs to be declared with two hashes otherwise drops out of scope immediately after dynamic sql statement creates it
-	SET @TableName = '#' + @TableName
+BEGIN
+	RAISERROR ('Table name must start with two hashes if it is to be temporary, to ensure that it does not drop out of scope immediately.', 16, -1)
+	RETURN -1
+END
 DECLARE @Database SYSNAME = CASE WHEN SUBSTRING(@TableName, 1, 1) = '#' THEN 'TempDB' ELSE DB_NAME() END
 DECLARE @Schema SYSNAME = 'dbo'
 DECLARE @FullTableName SYSNAME = @Database + '.' + @Schema + '.' + @TableName
@@ -74,7 +73,7 @@ SET @SQL = '
 BULK INSERT
 	#Header
 FROM
-	''' + REPLACE(@FromFile, '''', '''''') + '''
+	''' + REPLACE(@PathToFile, '''', '''''') + '''
 WITH (
 	FIRSTROW        = 1,
 	LASTROW         = 1,
@@ -104,7 +103,7 @@ BEGIN
 			ROLLBACK TRANSACTION before_import
 
 		--RAISERROR (120, -1, -1)
-		RAISERROR ('File contains a different number of fields than the header supplied', 16, -1)
+		RAISERROR ('File contains a different number of fields than the header supplied.', 16, -1)
 		RETURN -1
 	END
 END
@@ -145,7 +144,7 @@ BEGIN
 	ELSE
 		ROLLBACK TRANSACTION before_import
 	--RAISERROR (1038, 16, -1)
-	RAISERROR ('A field name specified in the header is empty', 16, -1)
+	RAISERROR ('A field name specified in the header is empty.', 16, -1)
 	RETURN -1
 END
 
@@ -157,28 +156,10 @@ IF @DebugMessages = 1
 EXEC (@SQL)
 
 IF @DebugMessages = 1
-	PRINT 'Table ' + @FullTableName + ' created successfully'
-
--- get the CardHash field from it's position
-IF ISNUMERIC(@CardHashField) = 1
-	SELECT TOP 1 @CardHashField = ColumnName
-	FROM @Columns
-	WHERE ColumnPosition = @CardHashField
-	OPTION (MAXDOP 1)
+	PRINT 'Table ' + @FullTableName + ' created successfully.'
 
 IF @DebugMessages = 1
 BEGIN
-	PRINT 'The field containing the card hash is called ' + @CardHashField
-	--PRINT 'Creating index on this field...'
-END
-
--- create index on CardHash field
---SET @SQL = 'CREATE INDEX [IX_' + @TableName + '_' + @CardHashField + '] ON ' + @UseTableName + ' ([' + @CardHashField + '])'
---EXEC (@SQL)
-
-IF @DebugMessages = 1
-BEGIN
-	--PRINT @CardHashField + ' index created.'
 	PRINT 'Importing data from file...'
 END
 
@@ -189,7 +170,7 @@ SET @SQL = '
 BULK INSERT
 	' + @UseTableName + '
 FROM
-	''' + REPLACE(@FromFile, '''', '''''') + '''
+	''' + REPLACE(@PathToFile, '''', '''''') + '''
 WITH (
 	FIRSTROW        = ' + CAST(1 + @FileContainsHeader AS VARCHAR) + ',
 	FIELDTERMINATOR = ''' + @Separator + ''',
@@ -201,73 +182,24 @@ WITH (
 EXEC (@SQL)
 
 IF @DebugMessages = 1
-	PRINT 'Data imported from file. Adding CardID column and index...'
+	PRINT 'Data imported from file.'
 
--- create CardID column in temporary table
-SET @SQL = 'ALTER TABLE ' + @UseTableName + ' ADD /*LineNumber INT IDENTITY(1, 1),*/ CardID int null' + CASE WHEN @ExtraColumns IS NULL THEN '' ELSE ', ' + @ExtraColumns END
-EXEC (@SQL)
-SET @SQL = 'CREATE INDEX IX_' + @TableName + '_CardID ON ' + @UseTableName + ' (CardID)'
-EXEC (@SQL)
-
-IF @DebugMessages = 1
-	PRINT 'Table updated.  Populating CardID field...'
-
--- splitting these into 2 separate queries drastically improves execution time!! (i.e. no LEFT JOIN to ECL)
-/*-- lookup the cards by the CardHash field
-SET @SQL = '
-UPDATE D
-SET CardID = Ca.CardID
-FROM ' + @UseTableName + '                  (NOLOCK) D
-LEFT OUTER JOIN PayLo.dbo.ExternalCardLoads (NOLOCK) ECL ON ECL.PanHash = D.[' + @CardHashField + '] COLLATE DATABASE_DEFAULT
-INNER JOIN      PayLo.dbo.Cards             (NOLOCK) Ca  ON (ECL.CardID IS NOT NULL AND Ca.CardID = ECL.CardID) OR (ECL.CardID IS NULL AND Ca.CardNumberHash = D.[' + @CardHashField + '] COLLATE DATABASE_DEFAULT)  
-OPTION (MAXDOP 1)'
-EXEC (@SQL)*/
-IF @DebugMessages = 1
-	PRINT '...from External Card Loads...'
-SET @SQL = '
-UPDATE D
-SET CardID = ECL.CardID
-FROM ' + @UseTableName + '             (NOLOCK) D
-INNER JOIN PayLo.dbo.ExternalCardLoads (NOLOCK) ECL ON ECL.PanHash = D.[' + @CardHashField + '] COLLATE DATABASE_DEFAULT
-OPTION (MAXDOP 1)'
-EXEC (@SQL)
-IF @DebugMessages = 1
-	PRINT '...from Cards...'
-SET @SQL = '
-UPDATE D
-SET CardID = Ca.CardID
-FROM ' + @UseTableName + '  (NOLOCK) D
-INNER JOIN PayLo.dbo.Cards  (NOLOCK) Ca ON Ca.CardNumberHash = D.[' + @CardHashField + '] COLLATE DATABASE_DEFAULT
-OPTION (MAXDOP 1)'
-EXEC (@SQL)
-
-IF @DebugMessages = 1
-	PRINT 'CardID field updated.'
-
-/*
--- output the mapped data if the table we created is temporary
-IF @DropTable = 1
+IF ISNULL(@ExtraColumns, '') != ''
 BEGIN
 	IF @DebugMessages = 1
-		PRINT 'Outputting contents of table ' + @FullTableName + ' and dropping it...'
-
-	IF @DebugMessages = 0
-		SET NOCOUNT ON
-
-	SET @SQL = 'SELECT *
-	FROM ' + @FullTableName + ' (NOLOCK)
-	OPTION (MAXDOP 1)
+		PRINT 'Adding extra columns specified...'
 	
-	DROP TABLE ' + @FullTableName
+	SET @SQL = 'ALTER TABLE ' + @UseTableName + ' ADD /*LineNumber INT IDENTITY(1, 1),*/ ' + @ExtraColumns -- commented out adding line number because doesn't seem to work as expected to use original order from file
 	EXEC (@SQL)
+	
+	IF @DebugMessages = 1
+		PRINT 'Table altered to include extra fields.'
 END
-*/
-
 
 SET NOCOUNT OFF
 IF @existingTranCount = 0
 	COMMIT TRANSACTION
 
 IF @DebugMessages = 1
-	PRINT 'End of stored procedure sp_ImportCardHashData.'
+	PRINT 'End of stored procedure proc_ImportCSVFile.'
 RETURN 0
