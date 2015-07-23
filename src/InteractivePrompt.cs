35c977a9469b895633bd8d50cf51e1aacf89c571
Keith Hall <kingkeith+github@gmail.com>
2015-07-23 16:37:48 +0300

    Update InteractivePrompt.cs

--- src/InteractivePrompt.cs
+++ src/InteractivePrompt.cs
@@ -3,6 +3,7 @@ using System.Windows.Forms;
 using System.Drawing;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace HallLibrary.Extensions
 {
