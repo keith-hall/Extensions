c8d5404f5d2010b4b97a7e906e0ee76bd3d5fd95
Keith Hall <kingkeith+github@gmail.com>
2015-07-29 12:15:29 +0300

    added svclog reader

--- src/WcfConsumerExtensions.cs
+++ src/WcfConsumerExtensions.cs
@@ -19,4 +19,92 @@ namespace HallLibrary.Extensions {
 			return new System.ServiceModel.MessageHeader<T>(value).GetUntypedHeader(name, nameSpace ?? string.Empty);
 		}
 	}
+	
+	public struct TraceData
+	{
+		public string Computer;
+		public DateTime TimeCreated;
+		public string Action;
+		public string MessageType;
+		public string Source;
+		public string Address;
+		public XElement Content;
+	}
+	
+	public static class TraceHelper {
+		public static IEnumerable<XElement> ElementsAnyNS<T>(this T source, string localName)
+			where T : XContainer
+		{
+			return source.Elements().Where(e => e.Name.LocalName == localName);
+		}
+		
+		public static XElement ElementAnyNS<T>(this T source, string localName)
+			where T : XContainer
+		{
+			return source.ElementsAnyNS(localName).FirstOrDefault();
+		}
+		
+		public static XAttribute AttributeAnyNS<T>(this T source, string localName)
+			where T : XElement
+		{
+			return source.Attributes().FirstOrDefault(a => a.Name.LocalName == localName);
+		}
+		
+		public static IEnumerable<TraceData> ReadTracesFromFile (string path)
+		{
+			// .svclog
+			var xr = XmlTextReader.Create(path, new XmlReaderSettings { ConformanceLevel = ConformanceLevel.Fragment });
+			//var lastWord = new Regex(@"[A-Z][a-z]+$");
+			var skipMessageTypes = new[] { "System.ServiceModel.Channels.NullMessage", "System.ServiceModel.Description.ServiceMetadataExtension+HttpGetImpl+MetadataOnHelpPageMessage" };
+			var skipActions = new[] { "http://tempuri.org/IConnectionRegister/",  "http://schemas.xmlsoap.org/" };
+			
+			xr.Read();
+			do {
+				if (xr.Name.Equals("E2ETraceEvent")) {
+					var xd = (XElement)XDocument.ReadFrom(xr);
+					
+					var traceData = new TraceData();
+					
+					var system = xd.ElementAnyNS("System");
+					traceData.TimeCreated = DateTime.Parse(system.ElementAnyNS("TimeCreated").AttributeAnyNS("SystemTime").Value);
+					traceData.Computer = system.ElementAnyNS("Computer").Value;
+					
+					var messageLog = xd.ElementAnyNS("ApplicationData").ElementAnyNS("TraceData").ElementAnyNS("DataItem").ElementAnyNS("MessageLogTraceRecord");
+					traceData.MessageType = messageLog.AttributeAnyNS("Type").Value;
+					if (skipMessageTypes.Contains(traceData.MessageType))
+						continue;
+					
+					var source = messageLog.AttributeAnyNS("Source").Value;
+					
+					traceData.Source = source;
+					
+					var soapEnvelope = messageLog.ElementAnyNS("Envelope");
+					if (soapEnvelope == null)
+					{
+						messageLog.Dump();
+					}
+					else
+					{
+						var header = soapEnvelope.ElementAnyNS("Header");
+						if (header == null)
+						{
+							traceData.Action = messageLog.ElementAnyNS("Addressing")?.ElementAnyNS("Action").Value;
+						}
+						else
+						{
+							traceData.Action = header.ElementAnyNS("Action")?.Value;
+	
+							traceData.Address = header.ElementAnyNS("To")?.Value;
+						}
+	
+						traceData.Content = soapEnvelope.ElementAnyNS("Body").Elements().FirstOrDefault();
+					}
+					
+					if (skipActions.Any(traceData.Action.StartsWith))
+						continue;
+					yield return traceData;
+				}
+			} while (xr.ReadToNextSibling("E2ETraceEvent"));
+		}
+	}
 }
