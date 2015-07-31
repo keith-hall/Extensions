2e66838be52d5800b87de0adf6aec91fddb90da9
Keith Hall <kingkeith+github@gmail.com>
2015-07-31 11:57:40 +0300

    Update WcfConsumerExtensions.cs

--- src/WcfConsumerExtensions.cs
+++ src/WcfConsumerExtensions.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Xml;
 using System.Linq;
+using System.Xml.Linq;
 
 namespace HallLibrary.Extensions {
 	public static class WcfConsumerExtensions {
