0397607ee56eccf3114a2e088a44511cbdfae151
Keith Hall <kingkeith+github@gmail.com>
2015-08-25 11:44:42 +0300

    Update EnumerableTests.cs

--- tests/EnumerableTests.cs
+++ tests/EnumerableTests.cs
@@ -10,44 +10,53 @@ namespace Tests
 	[TestClass]
 	public class EnumerableTests
 	{
+		private IEnumerable<int> _failAtFifthIteration = Enumerable.Range(1, 4).Concat(new[] { 0 }.Select(s => 1 / s));
+		private IEnumerable<int> _threeElements = Enumerable.Range(1, 3);
+		
 		[TestMethod]
 		public void TestCountExceeds()
 		{
-			var src = Enumerable.Range(1, 4).Concat(new[] { 0 }).Select(s => 1 / s);
-			// ReSharper disable once PossibleMultipleEnumeration
-			Assert.IsTrue(src.CountExceeds(3));
-
-			// just doing a Count would give a DivideByZero exception - this could also be demonstrated with the [ExpectedException(typeof(DivideByZeroException))] attribute on this method, but we want to test multiple conditions in a single method
-			try
-			{
-				// ReSharper disable once PossibleMultipleEnumeration
-				// ReSharper disable once ConvertIfStatementToConditionalTernaryExpression
-				if (src.Count() > 3)
-					Assert.Fail("The above should cause a DivideByZero Exception so this line should never be reached.");
-				else
-					Assert.Fail("Count is greater than 3 so this line should never be reached.");
-			}
-			catch (DivideByZeroException)
-			{
-
-			}
-			catch (Exception)
-			{
-				Assert.Fail("The above should cause a DivideByZero Exception so this line should never be reached.");
-			}
+			Assert.IsTrue(_failAtFifthIteration.CountExceeds(3));
 			
-			src = Enumerable.Range(1, 2);
-			Assert.IsFalse(src.CountExceeds(2));
+			Assert.IsTrue(_threeElements.CountExceeds(2));
+			Assert.IsFalse(_threeElements.CountExceeds(3));
+			Assert.IsFalse(_threeElements.CountExceeds(4));
+		}
+		
+		[TestMethod]
+		[ExpectedException(typeof(DivideByZeroException))]
+		public void TestCountException()
+		{
+			// show that Count iterates through all elements, which is why my extension methods are better
+			// ReSharper disable once ConvertIfStatementToConditionalTernaryExpression
+			if (_failAtFifthIteration.Count() > 3)
+				Assert.Fail("The above should cause a DivideByZero Exception so this line should never be reached.");
+			else
+				Assert.Fail("Count is greater than 3 so this line should never be reached.");
 		}
 		
 		[TestMethod]
 		public void TestCountEquals()
 		{
-			var src = Enumerable.Range(1, 3).Concat(Enumerable.Range(0, 1).Select(s => 1 / s));
-			Assert.IsFalse(src.Distinct().CountEquals(2));
-			
-			src = Enumerable.Range(1, 3);
-			Assert.IsTrue(src.CountEquals(3));
+			Assert.IsFalse(_failAtFifthIteration.CountEquals(2));
+			Assert.IsTrue(_threeElements.CountEquals(3));
+			Assert.IsFalse(_threeElements.CountEquals(2));
+		}
+		
+		[TestMethod]
+		public void TestCountIsLessThan()
+		{
+			Assert.IsFalse(_failAtFifthIteration.CountIsLessThan(3));
+			Assert.IsTrue(_threeElements.CountIsLessThan(4));
+			Assert.IsFalse(_threeElements.CountIsLessThan(3));
+		}
+		
+		[TestMethod]
+		public void TestDistinct()
+		{
+			// show that Distinct does not iterate through all elements before returning elements
+			Assert.IsFalse(_failAtFifthIteration.Distinct().CountEquals(2));
+			Assert.IsTrue(_threeElements.Distinct().CountEquals(3));
 		}
 	}
 }
