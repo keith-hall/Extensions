284e8d55216db65c6b0e9fc795eb127789a29cce
Keith Hall <keith-hall@users.noreply.github.com>
2016-06-23 15:19:31 +0300

    Update HttpPoller.cs

--- src/HttpPoller.cs
+++ src/HttpPoller.cs
@@ -10,30 +10,37 @@ namespace HallLibrary.Extensions
 	public static class HttpPoller
 	{
 		/// <summary>
-		/// Contains the response headers and plain text content from a web request.
+		/// Contains the response headers and plain text content from a web request, or details of the Exception that occurred as a result of the request.
 		/// </summary>
 		public struct ResponseDetails
 		{
 			public WebHeaderCollection Headers;
 			public string Text;
+			public WebException Exception;
 		}
-		
+	
 		/// <summary>
 		/// Poll a <paramref name="url"/> at the specified <paramref name="frequency"/> and create an observable that will update every time the content recieved changes.
 		/// </summary>
 		/// <param name="url">The url to poll.</param>
 		/// <param name="frequency">The frequency to poll at.</param>
 		/// <param name="createWebClient">An optional function to create a WebClient instance, for overriding timeouts, request headers etc.</param>
-		/// <returns>An observable containing the response headers and content of the specified url.</returns>
+		/// <returns>An observable containing the response headers and content of the specified url, or details of any exceptions that occurred.</returns>
 		/// <exception cref="ArgumentNullException"><paramref name="url" /> or <paramref name="frequency" /> are <c>null</c>.</exception>
-		/// <exception cref="WebException">A problem occurs polling the specified <paramref name="url"/>.</exception>
 		public static IObservable<ResponseDetails> PollURL(string url, TimeSpan frequency, Func<WebClient> createWebClient = null)
 		{
 			createWebClient = createWebClient ?? (() => new WebClient());
 			Func<ResponseDetails> download = () =>
 			{
-				var wc = createWebClient();
-				return new ResponseDetails { Text = wc.DownloadString(url), Headers = wc.ResponseHeaders };
+				try
+				{
+					var wc = createWebClient();
+					return new ResponseDetails { Text = wc.DownloadString(url), Headers = wc.ResponseHeaders };
+				}
+				catch (WebException ex)
+				{
+					return new ResponseDetails { Exception = ex };
+				}
 			};
 			return Observable.Interval(frequency)
 				.Select(l => download())
